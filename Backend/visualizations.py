de16fd241faed08eed004f63712927756cdd411e
chaudhary1337 <tanishqchaudhary511@gmail.com>
2021-06-22 13:39:48 -0400

    Add: better colors

--- Backend/visualizations.py
+++ Backend/visualizations.py
@@ -1,18 +1,20 @@
 import matplotlib.pyplot as plt
 
-plt.rcParams.update({
-    "figure.facecolor":  (1.0, 0.0, 0.0, 0.0),  # red   with alpha = 30%
-    "axes.facecolor":    (0.0, 1.0, 0.0, 0.0),  # green with alpha = 50%
-    "savefig.facecolor": (0.0, 0.0, 1.0, 0.0),  # blue  with alpha = 20%
-})
+plt.rcParams.update(
+    {
+        "figure.facecolor": (1.0, 0.0, 0.0, 0.0),  # red   with alpha = 30%
+        "axes.facecolor": (0.0, 1.0, 0.0, 0.0),  # green with alpha = 50%
+        "savefig.facecolor": (0.0, 0.0, 1.0, 0.0),  # blue  with alpha = 20%
+    }
+)
 
 
 def sentiment_piechart(sentiment_scores):
     print(sentiment_scores)
-    labels, frequencies = ['positive', 'neutral', 'negative'], []
+    labels, frequencies = ["positive", "neutral", "negative"], []
     for label in labels:
         frequencies.append(sentiment_scores[label])
-    colors = ['green', 'yellow', 'red']
+    colors = ["#A0EE2A", "#FFE000", "#F85634"]
     f, ax = plt.subplots()
     ax.pie(frequencies, labels=labels, colors=colors)
 
